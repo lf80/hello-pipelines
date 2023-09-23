ccc0f3844790a2f3a4db0c758ae1663c8fae5e32
Lukasz Super <lukaszsuper@hotmail.com>
2023-09-23 18:17:09 +0200

    Test

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -18,6 +18,7 @@ namespace WebApi
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
